967bb8c9d2903f0068b64ec512e7de0900b79f06
Larissa <larissa@Xense8.local>
2019-09-16 16:30:00 -0500

    Add Profile view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router  = express.Router();
+const {showProfile} = require('../controllers/profileController')
+const catchErrors = require('../middlewares/catchErrors')
+const isLoggedIn = require('../middlewares/isLoggedIn')
+const uploadCloud = require('../config/cloudinary')
+const User = require('../models/User')
+
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   res.render('index');
 });
+
+router.get('/profile', isLoggedIn('/auth/login'), catchErrors(showProfile))
 
 module.exports = router;
